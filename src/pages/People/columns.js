fbc9d303cd8c0582d07b0440a5eecdbaa6166b80
Madhav Kabra <madhav.kabra1100@gmail.com>
2023-05-05 21:12:29 +0530

    Get people data by api

---
+++ src/pages/People/columns.js
@@ -0,0 +1,54 @@
+export const columns = [
+  {
+    label: 'Sr No',
+    width: '5%',
+  },
+  {
+    label: 'Name',
+    width: '30%',
+  },
+  {
+    label: 'Gender',
+    width: '10%',
+  },
+  {
+    label: 'Birth Year',
+    width: '10%',
+  },
+  {
+    label: 'Eye Color',
+    width: '10%',
+  },
+  {
+    label: 'Hair Color',
+    width: '10%',
+  },
+  {
+    label: 'Skin Color',
+    width: '10%',
+  },
+  {
+    label: 'Height',
+    width: '10%',
+  },
+  {
+    label: 'Mass',
+    width: '10%',
+  },
+  {
+    label: 'Species',
+    width: '10%',
+  },
+  {
+    label: 'Films',
+    width: '10%',
+  },
+  {
+    label: 'Starships',
+    width: '10%',
+  },
+  {
+    label: 'Vehicles',
+    width: '10%',
+  },
+];
--- src/pages/People/index.jsx
+++ src/pages/People/index.jsx
@@ -1,61 +1,85 @@
 import React, { useState } from 'react';
+import useSWR from 'swr';
+
 import styles from './styles.module.css';
 import { TableToolbar } from '../../components/TableToolbar';
 import { Table } from '../../components/Table';
 import { TableFooter } from '../../components/TableFooter';
 import { TableContainer } from '../../components/TableContainer';
+import { API_BASE_URL } from '../../config';
+import { columns } from './columns';
 
-const columns = [
-  {
-    label: 'Sr No',
-    width: '',
-  },
-  {
-    label: 'Name',
-    width: '',
-  },
-  {
-    label: 'Email',
-    width: '',
-  },
-  {
-    label: 'Phone',
-    width: '',
-  },
-  {
-    label: 'Address',
-    width: '',
-  },
-];
-
-const rows = [
-  {
-    id: '01',
+const maniputatePeopleApiRes = (people) => {
+  return people.map((person, index) => ({
+    id: person.url,
     data: [
-      { label: '01' },
-      { label: 'Madhav' },
-      { label: '564654564' },
-      { label: 'madhav@gmail.com' },
-      { label: 'Indore' },
+      {
+        label: `${index + 1}`,
+      },
+      {
+        label: person.name,
+      },
+      {
+        label: person.gender,
+      },
+      {
+        label: person.birth_year,
+      },
+      {
+        label: person.eye_color,
+      },
+      {
+        label: person.hair_color,
+      },
+      {
+        label: person.skin_color,
+      },
+      {
+        label: person.height,
+      },
+      {
+        label: person.mass,
+      },
+      {
+        label: person.species.length,
+      },
+      {
+        label: person.films.length,
+      },
+      {
+        label: person.starships.length,
+      },
+      {
+        label: person.vehicles.length,
+      },
     ],
-  },
-  {
-    id: '02',
-    data: [
-      { label: '02' },
-      { label: 'Madhav K' },
-      { label: '68535664564' },
-      { label: 'madhavK@gmail.com' },
-      { label: 'Indore MP' },
-    ],
-  },
-];
+  }));
+};
 
 export const People = () => {
   const [searchedName, setSearchedName] = useState('');
+  const [apiEndpoint, setApiEndpoint] = useState('/api/people/');
+  const [page, setPage] = useState(1);
+
+  const { data } = useSWR(apiEndpoint, async () => {
+    const res = await fetch(`${API_BASE_URL}${apiEndpoint}`);
+    const data = await res.json();
+
+    return { ...data, results: maniputatePeopleApiRes(data.results) };
+  });
 
   const handleSearchNameChange = (event) => {
     setSearchedName(event.target.value);
+  };
+
+  const handleNextButtonClick = () => {
+    setApiEndpoint(`/api/people/?page=${page + 1}`);
+    setPage((page) => page + 1);
+  };
+
+  const handlePreviousButtonClick = () => {
+    setApiEndpoint(`/api/people/?page=${page - 1}`);
+    setPage((page) => page - 1);
   };
 
   return (
@@ -74,14 +98,16 @@ export const People = () => {
         {/* Table */}
         <Table
           columns={columns}
-          rows={rows}
+          rows={data?.results || []}
         />
 
         {/* Table footer */}
         <TableFooter
-          currentPage={0}
-          onNext={null}
-          onPrevious={null}
+          currentPage={page}
+          disabledNext={!data?.next}
+          disabledPrevious={!data?.previous}
+          onNext={handleNextButtonClick}
+          onPrevious={handlePreviousButtonClick}
         />
       </TableContainer>
     </div>
