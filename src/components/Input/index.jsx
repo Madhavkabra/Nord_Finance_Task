3c68e702fb689ed19082704df6386cbbc046c05e
Madhav Kabra <madhav.kabra1100@gmail.com>
2023-05-05 19:04:18 +0530

    Add input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import styles from './styles.module.css';
+import { useOutsideClickListener } from '../../hooks/useClickListener';
+
+export const Input = ({ name, value, onChange, placeholder, ...props }) => {
+  const [focused, setFocused] = useState(false);
+  const inputRef = useRef();
+
+  useOutsideClickListener({
+    ref: inputRef,
+    onClickOutside: () => setFocused(false),
+  });
+
+  const handleInputFocus = () => {
+    setFocused(true);
+  };
+
+  const handleClearInputButtonClick = () => {
+    onChange({ target: { value: '' } });
+  };
+
+  return (
+    <div
+      ref={inputRef}
+      className={cx(styles.inputContainer, {
+        [styles.inputContainerFocused]: focused,
+      })}
+      onClick={handleInputFocus}
+    >
+      <input
+        className={styles.input}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        ref={(ref) => (focused && ref ? ref.focus() : ref)}
+        {...props}
+      />
+
+      {/* Clear input button */}
+      {value && (
+        <div
+          className={styles.inputIconContainer}
+          onClick={handleClearInputButtonClick}
+        >
+          <i class='fa-solid fa-xmark fa-lg'></i>
+        </div>
+      )}
+    </div>
+  );
+};
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+};
